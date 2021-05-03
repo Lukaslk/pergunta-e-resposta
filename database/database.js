553cf62d3b31037073dbb031cb2e9d5d2b390085
Lukaslk <50305227+Lukaslk@users.noreply.github.com>
2021-05-03 09:32:37 -0300

    .

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const conn = new Sequelize('db_perguntas', 'root', 'Chave$$12', {
+const conn = new Sequelize('db_perguntas', 'root', 'senha', {
     hots: 'localhost',
     dialect: 'mysql'
 })
